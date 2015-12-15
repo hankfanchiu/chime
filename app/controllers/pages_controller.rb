85cea8097009039c44bc09ae6dc73b5af165a889
Hank Fanchiu <hank@fanchiu.com>
2015-12-14 23:57:14 -0800

    Add user & session routing; pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def root
+    render :root
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Rails.application.routes.draw do
+  root to: "pages#root"
+
+  resource :users, only: [:create, :show]
+  resource :session, only: [:create, :destroy]
 end
