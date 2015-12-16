595db53c648ecb9b0eaf02e22b1165008444b95a
Hank Fanchiu <hank@fanchiu.com>
2015-12-15 16:06:35 -0800

    Add model annotation

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tracks
+#
+#  id          :integer          not null, primary key
+#  artist_id   :integer          not null
+#  title       :string           not null
+#  track_url   :string           not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Track < ActiveRecord::Base
   validates_presence_of :artist_id, :title
 
