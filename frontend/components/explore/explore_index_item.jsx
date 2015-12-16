13e767581d00a2d356002f6d40ed06af54d68962
Hank Fanchiu <hank@fanchiu.com>
2015-12-15 21:38:59 -0800

    Format Explore grids; add CSS

--- frontend/components/explore/explore_index_item.jsx
+++ frontend/components/explore/explore_index_item.jsx
@@ -3,13 +3,17 @@ var React = require("react");
 var ExploreIndexItem = React.createClass({
   render: function () {
     return (
-      <div className="col-xs-2 explore-index-item">
-        <div className="image">
-          <img src="/assets/corgi.jpg" />
-        </div>
+      <div className="col-xs-3 explore-index-item">
+        <div className="box">
+          
+          <div className="image">
+            <img src="/assets/corgi.jpg" />
+          </div>
 
-        <div className="title">
-          { this.props.track.title }
+          <div className="title">
+            { this.props.track.title }
+          </div>
+
         </div>
       </div>
     );
