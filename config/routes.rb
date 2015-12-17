529805984d97f30d5bd8681be4afd7afe656775f
Hank Fanchiu <hank@fanchiu.com>
2015-12-16 17:48:45 -0800

    Add Playlist routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :tracks, except: [:new, :edit]
+    resources :playlists, except: [:new, :edit]
   end
 end
