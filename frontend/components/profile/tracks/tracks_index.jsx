d24bf0a51596e8bf31f85b616ba0b80e47d0958a
Hank Fanchiu <hank@fanchiu.com>
2015-12-19 17:10:52 -0800

    Reffactor Profile Tracks component to remove need for root 'Tracks'

--- frontend/components/profile/tracks/tracks_index.jsx
+++ frontend/components/profile/tracks/tracks_index.jsx
@@ -1,9 +1,48 @@
 var React = require("react");
+var ProfileStore = require("../../../stores/profile_store");
 var TracksIndexItem = require("./tracks_index_item");
 
 var TracksIndex = React.createClass({
-  tracksIndexItems: function () {
-    return this.props.tracks.map(function (track, idx) {
+  getInitialState: function () {
+    return { tracks: ProfileStore.getTracks() };
+  },
+
+  componentDidMount: function () {
+    this.listenerToken = ProfileStore.addListener(this._onChange);
+  },
+
+  componentWillUnmount: function () {
+    this.listenerToken.remove();
+  },
+
+  _onChange: function () {
+    this.setState({ tracks: ProfileStore.getTracks() });
+  },
+
+  _goToForm: function () {
+    return;
+  },
+
+  renderIndexStatus: function () {
+    if (this.state.tracks.length === 0) {
+      return (
+        <h4>
+          You have no tracks! <a onClick={ this._goToForm }>Add a track to get started.</a>
+      </h4>
+      );
+    } else {
+      return (
+        <h4>
+          <a onClick={ this._goToForm }>
+            <i className="fa fa-plus"></i> Add Track
+          </a>
+        </h4>
+      )
+    }
+  },
+
+  renderTracksIndexItems: function () {
+    return this.state.tracks.map(function (track, idx) {
       return <TracksIndexItem key={ idx } track={ track } />;
     });
   },
@@ -11,8 +50,9 @@ var TracksIndex = React.createClass({
   render: function () {
     return (
       <div className="tracks-index clear">
+        { this.renderIndexStatus() }
 
-        { this.tracksIndexItems() }
+        { this.renderTracksIndexItems() }
 
       </div>
     );
--- frontend/router.jsx
+++ frontend/router.jsx
@@ -8,9 +8,9 @@ var App = require("./components/app");
 var Home = require("./components/home");
 var Discover = require("./components/discover/discover");
 var Search = require("./components/search/search");
-var ProfileIndex = require("./components/profile/profile_index");
-var Tracks = require("./components/profile/tracks/tracks");
-var Playlists = require("./components/profile/playlists/playlists_index");
+var Profile = require("./components/profile/profile");
+var TracksIndex = require("./components/profile/tracks/tracks_index");
+var PlaylistsIndex = require("./components/profile/playlists/playlists_index");
 var Settings = require("./components/profile/settings");
 var Login = require("./components/session/login");
 var SignUp = require("./components/session/sign_up");
@@ -23,10 +23,10 @@ module.exports = (
       <Route name="discover" path="/discover" component={ Discover } />
       <Route name="search" path="/search" component={ Search } />
 
-      <Route name="profile" path="/profile" component={ ProfileIndex }>
-        <Route name="tracks" path="/tracks" component={ Tracks } />
+      <Route name="profile" path="/profile" component={ Profile }>
+        <Route name="tracks" path="/tracks" component={ TracksIndex } />
         <Route name="playlists" path="/playlists"
-          component={ Playlists } />
+          component={ PlaylistsIndex } />
       </Route>
 
       <Route name="settings" path="/settings" component={ Settings } />
