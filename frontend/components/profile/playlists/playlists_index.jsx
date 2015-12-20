de19d0c614291049be2e0da0f7b220e29657f85f
Hank Fanchiu <hank@fanchiu.com>
2015-12-19 17:00:20 -0800

    Refactor Profile Playlists component to remove need for root 'Playlists'

--- frontend/components/profile/playlists/playlists_index.jsx
+++ frontend/components/profile/playlists/playlists_index.jsx
@@ -1,18 +1,39 @@
 var React = require("react");
+var ProfileStore = require("../../../stores/profile_store");
 var PlaylistsIndexItem = require("./playlists_index_item");
 
 var PlaylistsIndex = React.createClass({
-  playlistsIndexItems: function () {
-    return this.props.playlists.map(function (playlist, idx) {
-      return <PlaylistsIndexItem key={ idx } playlist={ playlist } />;
-    });
+  getInitialState: function () {
+    return { playlists: ProfileStore.getPlaylists() };
+  },
+
+  componentDidMount: function () {
+    this.listenerToken = ProfileStore.addListener(this._onChange);
+  },
+
+  componentWillUnmount: function () {
+    this.listenerToken.remove();
+  },
+
+  _onChange: function () {
+    this.setState({ playlists: ProfileStore.getPlaylists() });
+  },
+
+  renderPlaylistsIndexItems: function () {
+    if (this.state.playlists.length === 0) {
+      return <p>You have no playlists!</p>;
+    } else {
+      return this.state.playlists.map(function (playlist, idx) {
+        return <PlaylistsIndexItem key={ idx } playlist={ playlist } />;
+      });
+    }
   },
 
   render: function () {
     return (
       <div className="playlists-index clear">
 
-        { this.playlistsIndexItems() }
+        { this.renderPlaylistsIndexItems() }
 
       </div>
     );
