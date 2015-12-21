73d8c7f27bd935c713aa35b215af8646d94bbc73
Hank Fanchiu <hank@fanchiu.com>
2015-12-20 23:35:10 -0800

    Add full CRUD API functions to TrackAPIUtils

--- frontend/utils/track_api_utils.js
+++ frontend/utils/track_api_utils.js
@@ -3,8 +3,50 @@ var TrackAPIUtils = {
     $.ajax({
       url: "/api/tracks",
       type: "GET",
-      success: function (tracks) {
-        actionCallback(tracks);
+      success: function (response) {
+        actionCallback(response);
+      }
+    });
+  },
+
+  fetchTrack: function (trackTitle, actionCallback) {
+    $.ajax({
+      url: "/api/tracks/" + trackTitle,
+      type: "GET",
+      success: function (response) {
+        actionCallback(response);
+      }
+    });
+  },
+
+  createTrack: function (trackData, actionCallback) {
+    $.ajax({
+      url: "/api/tracks",
+      type: "POST",
+      data: {track: trackData},
+      success: function (response) {
+        actionCallback(response);
+      }
+    });
+  },
+
+  updateTrack: function (trackTitle, trackData, actionCallback) {
+    $.ajax({
+      url: "/api/tracks/" + trackTitle,
+      type: "PATCH",
+      data: {track: trackData},
+      success: function (response) {
+        actionCallback(response);
+      }
+    });
+  },
+
+  deleteTrack: function (trackTitle, actionCallback) {
+    $.ajax({
+      url: "/api/tracks/" + trackTitle,
+      type: "DELETE",
+      success: function (response) {
+        actionCallback(response);
       }
     });
   }
