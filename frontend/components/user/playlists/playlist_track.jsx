0b321ee3102c3fc9ff949c7dbe99e5bd7331dbd3
Hank Fanchiu <hank@fanchiu.com>
2015-12-25 18:38:16 -0800

    Restylize User > Playlists index

--- frontend/components/user/playlists/playlist_track.jsx
+++ frontend/components/user/playlists/playlist_track.jsx
@@ -2,7 +2,8 @@ var React = require("react");
 var ListGroupItem = require("react-bootstrap").ListGroupItem;
 var Row = require("react-bootstrap").Row;
 var Col = require("react-bootstrap").Col;
-var Thumbnail = require("react-bootstrap").Thumbnail;
+var Image = require("react-bootstrap").Image;
+var Glyphicon = require("react-bootstrap").Glyphicon;
 var PlayerActions = require("../../../actions/player_actions");
 
 var PlaylistTrack = React.createClass({
@@ -14,22 +15,25 @@ var PlaylistTrack = React.createClass({
     var track = this.props.track;
 
     return (
-      <ListGroupItem>
+      <ListGroupItem onClick={ this._playTrack }>
         <Row>
-          <Col xs={ 2 } sm={ 2 } md={ 2 }>
-            <Thumbnail src={ track.img_hero }
-              onClick={ this._playTrack } />
+          <Col xs={ 1 } sm={ 1 } md={ 1 }>
+            <Image src={ track.img_hero } style={{ width: "30px" }} />
           </Col>
 
-          <Col xs={ 10 } sm={ 10 } md={ 10 }>
+          <Col xs={ 11 } sm={ 11 } md={ 11 }>
             <Row>
               <Col xs={ 1 } sm={ 1 } md={ 1 }>
                 { this.props.index }
               </Col>
 
-              <Col xs={ 11 } sm={ 11 } md={ 11 }>
+              <Col xs={ 10 } sm={ 10 } md={ 10 }>
                 { track.user.username }<br/>
                 { track.title }
+              </Col>
+
+              <Col xs={ 1 } sm={ 1 } md={ 1 }>
+                <Glyphicon glyph="play" />
               </Col>
             </Row>
 
