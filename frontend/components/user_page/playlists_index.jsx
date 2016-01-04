6d15b87ff32fc9e16f9b95a804daea4686cccad2
Hank Fanchiu <hank@fanchiu.com>
2016-01-04 13:33:20 -0800

    Don't fetch for tracks/playlists on user change

--- frontend/components/user_page/playlists_index.jsx
+++ frontend/components/user_page/playlists_index.jsx
@@ -29,15 +29,6 @@ var PlaylistsIndex = React.createClass({
     PlaylistActions.fetchPlaylists(this.props.params.username);
   },
 
-  componentWillReceiveProps: function (nextProps) {
-    var nextUser = nextProps.params.username;
-    var sameUser = (this.props.params.username === nextUser);
-
-    if (!sameUser) {
-      PlaylistActions.fetchPlaylists(nextUser);
-    }
-  },
-
   componentWillUnmount: function () {
     this.playlistListener.remove();
     this.sessionListener.remove();
